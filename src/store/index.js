0355dd678336e85c6733d3faa8b21760e2e2939e
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-24 21:47:56 +0300

    createSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; 
+      // it seems like we're mutating the state here directly, but Redux Toolkit will create another state copy behind the hood, so in this case writing it like this is allowed
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
@@ -8,7 +29,10 @@ const counterReducer = (state = initialState, action) => {
   }
 
   if (action.type === "incrementByX") {
-    return { counter: state.counter + action.amount, showCounter: state.showCounter };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "decrement") {
